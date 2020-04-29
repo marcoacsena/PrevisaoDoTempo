89de92181d76e43ecb31e6548273c50583979717
MarcoSena <marcoacsena@gmail.com>
2020-04-29 16:35:15 -0300

    Anotação (@SerializedName) ajustada para o forecast

--- app/src/main/java/com/example/previsaodotempo/retrofit/PrevisaoDoTempoApi.java
+++ app/src/main/java/com/example/previsaodotempo/retrofit/PrevisaoDoTempoApi.java
@@ -10,17 +10,10 @@ import retrofit2.http.Query;
 
 public interface PrevisaoDoTempoApi {
 
-    //@GET("weather?key=3ec0c5f0&city_name={nomeDaCidade}")
-    //Call<PrevisaoDoTempo> getPrevisaoDoTempo(@Path("nomeDaCidade") String nomeDaCidade);
 
     @GET("/weather?array_limit=8&fields=only_results,temp,date,Florianopolis,forecast,max,min,date, weekday&key=b6411d18")
     Call<PrevisaoDoDia> getPrevisaoDoTempo();
 
-    //"weather?array_limit=2&fields=only_results,temp,Sao Paulo,forecast,max,min,date&key=b6411d18"
 
-//"?array_limit=2&fields=only_results,temp,city_name,forecast,max,min,date&key=909760a2"
-
-//    @GET("weather?key=3ec0c5f0/{city_name}") @Query("nomeDaCidade") String nomeDaCidade
-//    Call<PrevisaoDoTempo> getPrevisaoDoTempo (@Path("city_name") String nomeCidade);
 }
 
